b8f6452744666a7d58031d314d543f62cf7d6dac
elvisbevab <elvis.bevab@mailbox.org>
2023-06-09 16:54:20 +0200

    sizing of icons and respnsiveness finished'

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -9,59 +9,52 @@ import {
 import './css/contactInfo.css';
 
 function ContactInfo() {
+  const iconSize = 28;
   return (
     <Container className='contactInfo-container text-start p-5 fa-lg'>
       <Row>
         <h2 className=''>Contact us</h2>
       </Row>
-      <Row className='pt-5 pb-4'>
-        <p>We're open for any suggestion or just to have a chat</p>
+      <Row className='pt-5 pb-3'>
+        We're open for any suggestion or just to have a chat
       </Row>
       <Row className=''>
-        <Col className='bla' style={{ padding: 0 }} xs={2}>
-          <BsFillGeoAltFill />
+        <Col className='center-icon' style={{ padding: 0 }} xs={2}>
+          <BsFillGeoAltFill size={iconSize} />
         </Col>
         <Col>
-          <p>
-            <span>Adress: </span>
-            Hauptstrasse 22 12345 Oberhausen
-          </p>
+          <span>Adress: </span>
+          Hauptstrasse 22 12345 Oberhausen
         </Col>
       </Row>
 
-      <Row className='pt-4'>
-        <Col style={{ padding: 0 }} xs={2}>
-          <BsTelephoneFill size={30} />
+      <Row className='pt-5'>
+        <Col className='center-icon' style={{ padding: 0 }} xs={2}>
+          <BsTelephoneFill size={iconSize} />
         </Col>
         <Col>
-          <p>
-            <span>Phone: </span>
-            +49 171 8473938
-          </p>
+          <span>Phone: </span>
+          +49 171 8473938
         </Col>
       </Row>
 
-      <Row className='pt-4'>
-        <Col xs={2}>
-          <BsFillSendFill size={30} />
+      <Row className='pt-5 flex-nowrap'>
+        <Col className='center-icon' style={{ padding: 0 }} xs={2}>
+          <BsFillSendFill size={iconSize} />
         </Col>
         <Col>
-          <p>
-            <span>Email: </span>
-            info@cookbook.com
-          </p>
+          <span>Email: </span>
+          info@cookbook.com
         </Col>
       </Row>
 
-      <Row className='pt-4'>
-        <Col xs={2}>
-          <BsGlobeCentralSouthAsia size={30} />
+      <Row className='pt-5'>
+        <Col className='center-icon' style={{ padding: 0 }} xs={2}>
+          <BsGlobeCentralSouthAsia size={iconSize} />
         </Col>
         <Col>
-          <p>
-            <span>Website: </span>
-            cookbook.com
-          </p>
+          <span>Website: </span>
+          cookbook.com
         </Col>
       </Row>
     </Container>
