fdf83b00e421ab0b9d5aa90ab49411ff1fb40378
elvisbevab <elvis.bevab@mailbox.org>
2023-06-09 23:17:51 +0200

    parent surrounding for return in RecipeList

--- src/pages/RecipeList.jsx
+++ src/pages/RecipeList.jsx
@@ -1,63 +1,67 @@
-import { useEffect, useState } from "react";
-import styled from "styled-components";
-import { client } from "../client";
-import RecipeView from "../components/RecipeView";
-import { useParams, Link } from "react-router-dom";
-import RecipeCard from "../components/RecipeCard"
-
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { client } from '../client';
+import RecipeView from '../components/RecipeView';
+import { useParams, Link } from 'react-router-dom';
+import RecipeCard from '../components/RecipeCard';
 
 export default function RecipeList() {
   const [recipes, setRecipes] = useState([]);
-  let {id} = useParams();
+  let { id } = useParams();
 
   useEffect(() => {
     client
       .getEntries()
       .then((response) => {
-        console.log(response.items, "hurra :)");
+        console.log(response.items, 'hurra :)');
         setRecipes(response.items);
       })
       .catch((err) => console.log(err));
   }, []);
 
   const handleRecipeClick = (recipeId) => {
-    id = recipeId
-    console.log("Recipe clicked with ID:", recipeId);
-  }
+    id = recipeId;
+    console.log('Recipe clicked with ID:', recipeId);
+  };
   return (
-
-    <div>
+    <>
       <div>
-        {recipes.map((recipe) => {
-          return <Link key={recipe.sys.id} to={`/recipes/${recipe.sys.id}`} ><button >{recipe.fields.title}</button></Link>;
-        })}
+        <div>
+          {recipes.map((recipe) => {
+            return (
+              <Link key={recipe.sys.id} to={`/recipes/${recipe.sys.id}`}>
+                <button>{recipe.fields.title}</button>
+              </Link>
+            );
+          })}
+        </div>
       </div>
-      
-    </div>
 
-<CardsContainer>
-    <CardGrid>
-      {recipes.map((recipe) => {
-        return <div key={recipe.sys.id}>
-          <RecipeCard recipe={recipe}/>
-        </div>;
-      })}
-    </CardGrid>
- </CardsContainer>
-
+      <CardsContainer>
+        <CardGrid>
+          {recipes.map((recipe) => {
+            return (
+              <div key={recipe.sys.id}>
+                <RecipeCard recipe={recipe} />
+              </div>
+            );
+          })}
+        </CardGrid>
+      </CardsContainer>
+    </>
   );
 }
 
 const CardsContainer = styled.div`
-margin-top: 3em;
-display: flex;
-justify-content: center;
-`
+  margin-top: 3em;
+  display: flex;
+  justify-content: center;
+`;
 
 const CardGrid = styled.div`
-width: 90%;
-display: grid;
+  width: 90%;
+  display: grid;
   gap: 2rem;
   grid-template-columns: repeat(auto-fit, minmax(12em, 1fr));
-justify-content: center;
-`
+  justify-content: center;
+`;
