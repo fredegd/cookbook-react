fd3157fc751f76208332eea3513faa6a0ed25ddd
elvisbevab <elvis.bevab@mailbox.org>
2023-06-13 15:29:21 +0200

    about cards centered and gap added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus sequi sit asperiores dolor nam vel, porro nihil odio. Rerum, enim!Lorem ipsum, dolor sit amet consectetur adipisicing elit';
   return (
     <Container fluid>
-      <Row className='person-container df-flex-md-row justify-content-around my-5'>
+      <Row className='person-container df-flex-md-row justify-content-center gap-5 my-5'>
         <PersonCard title='Elvis B' body={elvis} imgSrc='elvisB.png' />
         <PersonCard title='Anne P' body={elvis} imgSrc='anneP.png' />
         <PersonCard title='Fred ' body={elvis} imgSrc='fred.jpg' />
