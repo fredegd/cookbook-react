1652f4da568524c82117c0413ed21a4272d91326
fredegd <124848643+fredegd@users.noreply.github.com>
2023-06-13 20:18:15 +0200

    red

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
 import { Card } from 'react-bootstrap';
+import { cls } from "../colors";
 import './css/personCard.css';
 
 function PersonCard({ title, body, imgSrc }) {
+
   return (
     <Card className='my-3' style={{ width: '20rem' }}>
-      <Card.Img variant='top' className='mt-4' src={`/src/images/${imgSrc}`} />
+      <Card.Img variant='top' className='mt-4' src={imgSrc} />
       <Card.Body className='card-myBody'>
         <Card.Title>{title}</Card.Title>
         <Card.Text>{body}</Card.Text>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,33 @@
-import { Container, Row } from 'react-bootstrap';
-import PersonCard from '../components/PersonCard';
+import { useEffect, useState } from "react";
+import { client } from "../client";
+import { Container, Row } from "react-bootstrap";
+import PersonCard from "../components/PersonCard";
 
 export default function About() {
-  const elvis =
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus sequi sit asperiores dolor nam vel, porro nihil odio. Rerum, enim!Lorem ipsum, dolor sit amet consectetur adipisicing elit';
-  return (
+  const [authors, setAuthors] = useState([]);
+
+  useEffect(() => {
+    client
+      .getEntries({
+        content_type: "cookbookAuthor",
+      })
+      .then((response) => {
+        // console.log(response.items, "huhu")
+        setAuthors(response.items);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  console.log(authors);
+
+ return (
     <Container fluid>
-      <Row className='person-container df-flex-md-row justify-content-center gap-5 py-5'>
-        <PersonCard title='Elvis B' body={elvis} imgSrc='elvisB.png' />
-        <PersonCard title='Anne P' body={elvis} imgSrc='anneP.png' />
-        <PersonCard title='Fred ' body={elvis} imgSrc='fred.jpg' />
+      <Row className="person-container df-flex-md-row justify-content-center gap-5 py-5">
+        {authors.map((author) => {
+          return (
+            <PersonCard title={author.fields.authorName} body={author.fields.authorDescription} imgSrc={author.fields.authorImg.fields.file.url} />
+          );
+        })}
       </Row>
     </Container>
   );
